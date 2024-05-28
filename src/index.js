173eb36fed62f382298281f0eb455e1acb22521a
VedSher <138235974+VedSher@users.noreply.github.com>
2024-05-28 16:42:52 +0530

    Connected to mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+//require ('dotenv').config({path: './env'});
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
-export default index
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js"
+import express from "express"
+const app = express()
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB();
+
+
+
+// ( async () => {
+//   try {
+//     await mongoose.connect(`${process.env.
+//       MONGO_URL}/${DB_NAME}`)
+//       app.on("error", (err) => {
+//         console.log("Error aya hai: ",err);
+//         throw err
+//       })
+
+//       app.listen(process.env.PORT, () => {
+//         console.log(`App is Listening on port ${process.env.PORT}`);
+//       })
+//   }catch (err){
+//     console.error("ERROR: ", err);
+//     throw err
+//   }
+// }) ()
