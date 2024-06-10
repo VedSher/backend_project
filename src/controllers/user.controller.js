3e7dc4f27ea4409b67aab05ae44c2c63f63af864
VedSher <138235974+VedSher@users.noreply.github.com>
2024-06-10 12:13:33 +0530

    add Register Controller and Bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,86 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiRespone } from "../utils/ApiResponse.js";
+
 
 const registerUser = asyncHandler( async (req, res) => {
-    return res.status(500).json({
-        message: "Maza hi Maza haiii"
+    // get user details from front-end(done)
+    //email aur username empty toh nai bheja(done)
+    // check if user exist?using email/username(done)
+    //if our files(imgs aur avatar) are filled or not(done)
+    //upload them to cloudnary(done)
+    //create user object - create entry in db(done)
+    //remove password & refresh token field from response
+    //check for user creation
+    //return res
+    
+    const { fullName, email, username, password } = req.body
+    function isValidEmail(e) {
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(e);
+      }
+      if (isValidEmail(email)) {
+          console.log("email : ", email);
+      } else {
+        console.log("Please enter a valid email!!")
+      }
+    if (fullName === ""){
+        throw new ApiError(400, "fullname is required" )
+    }
+
+    console.log("full-Name : ", fullName);
+    
+    if (
+        [fullName, email, username, password].some((field) =>
+         field?.trim() === "" )
+    ) {
+        throw new ApiError(400, "All field are required")
+    }
+    const existedUser = User.findOne({
+      $or: [{username}, {email}]
     })
+
+    if(existedUser) {
+      throw new ApiError(409, "User with email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    
+    if (!avatar) {
+      throw new ApiError(400, "Avatar file is required");
+    }
+
+    const user = await User.create({
+      fullName,
+      avatar: avatar.url,
+      covarImage: coverImage.url || "",
+      email,
+      password,
+      username: username.toLowerCase()
+    })
+
+    const checkUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!checkUser){
+      throw new ApiError(500, "Error in registering the user")
+    }
+
+    return res.status(201).json(
+      new ApiRespone(200, checkUser, "User registed Successfully")
+    )
+
 } )
 
 export { registerUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
-//http://localhost:8000/users/register
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+//http://localhost:8000/api/v1/users/register
 
 export default router;
